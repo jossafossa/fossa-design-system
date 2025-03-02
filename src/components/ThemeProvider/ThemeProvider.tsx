a43fb5bc803c23bf76e04100e2b7762abe38122a
jossafossa <joosthobma@gmail.com>
2025-03-02 23:31:32 +0100

    theme provider

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,5 +1,30 @@
-import { createContext } from 'react';
+import { createContext, PropsWithChildren, useContext } from 'react';
 
-const ThemeContext = createContext(undefined);
+const ThemeContext = createContext<Theme | undefined>(undefined);
 
-export const ThemeProvider = ThemeContext.Provider;
+type Theme = {
+  tokens: { [key: string]: string };
+};
+
+type ThemeProviderProps = {
+  theme: Theme;
+};
+
+export const ThemeProvider = ({ theme, children }: PropsWithChildren<ThemeProviderProps>) => {
+  const { tokens } = theme;
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const theme = useContext(ThemeContext);
+
+  if (!theme) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return theme;
+};
